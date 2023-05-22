36a96ff12d55dea6f44dd6620edf11413354a7dd
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-05-22 09:47:03 -0600

    Commit servlets

--- src/main/java/dataAccessLayer/EmbeddedNeo4j.java
+++ src/main/java/dataAccessLayer/EmbeddedNeo4j.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package dataAccessLayer;
 
 import org.neo4j.driver.AuthTokens;
@@ -17,122 +14,102 @@ import static org.neo4j.driver.Values.parameters;
 
 import java.util.LinkedList;
 import java.util.List;
-/**
- * @author Administrator
- *
- */
-public class EmbeddedNeo4j implements AutoCloseable{
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class EmbeddedNeo4j implements AutoCloseable {
 
     private final Driver driver;
-    
 
-    public EmbeddedNeo4j( String uri, String user, String password )
-    {
-        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    public EmbeddedNeo4j(String uri, String user, String password) {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
     }
 
     @Override
-    public void close() throws Exception
-    {
+    public void close() throws Exception {
         driver.close();
     }
 
-    public void printGreeting( final String message )
-    {
-        try ( Session session = driver.session() )
-        {
-            String greeting = session.writeTransaction( new TransactionWork<String>()
-            {
-                @Override
-                public String execute( Transaction tx )
-                {
-                    Result result = tx.run( "CREATE (a:Greeting) " +
-                                                     "SET a.message = $message " +
-                                                     "RETURN a.message + ', from node ' + id(a)",
-                            parameters( "message", message ) );
-                    return result.single().get( 0 ).asString();
+    public void printGreeting(String message) {
+        try (Session session = driver.session()) {
+            CompletableFuture<Void> greeting = CompletableFuture.supplyAsync(() -> {
+                try (Transaction tx = session.beginTransaction()) {
+                    Result result = tx.run(
+                            "CREATE (a:Greeting) " +
+                                    "SET a.message = $message " +
+                                    "RETURN a.message + ', from node ' + id(a)",
+                            parameters("message", message)
+                    );
+                    ResultSummary summary = result.consume();
+                    tx.commit();
                 }
-            } );
-            System.out.println( greeting );
-        }
-    }
-    
-    public LinkedList<String> getActors()
-    {
-    	 try ( Session session = driver.session() )
-         {
-    		 
-    		 
-    		 LinkedList<String> actors = session.readTransaction( new TransactionWork<LinkedList<String>>()
-             {
-                 @Override
-                 public LinkedList<String> execute( Transaction tx )
-                 {
-                     Result result = tx.run( "MATCH (people:Person) RETURN people.name");
-                     LinkedList<String> myactors = new LinkedList<String>();
-                     List<Record> registros = result.list();
-                     for (int i = 0; i < registros.size(); i++) {
-                    	 //myactors.add(registros.get(i).toString());
-                    	 myactors.add(registros.get(i).get("people.name").asString());
-                     }
-                     
-                     return myactors;
-                 }
-             } );
-             
-             return actors;
-         }
-    }
-    
-    public LinkedList<String> getMoviesByActor(String actor)
-    {
-   	 try ( Session session = driver.session() )
-        {
-   		 
-   		 
-   		 LinkedList<String> actors = session.readTransaction( new TransactionWork<LinkedList<String>>()
-            {
-                @Override
-                public LinkedList<String> execute( Transaction tx )
-                {
-                    Result result = tx.run( "MATCH (tom:Person {name: \"" + actor + "\"})-[:ACTED_IN]->(actorMovies) RETURN actorMovies.title");
-                    LinkedList<String> myactors = new LinkedList<String>();
-                    List<Record> registros = result.list();
-                    for (int i = 0; i < registros.size(); i++) {
-                   	 //myactors.add(registros.get(i).toString());
-                   	 myactors.add(registros.get(i).get("actorMovies.title").asString());
-                    }
-                    
-                    return myactors;
-                }
-            } );
-            
-            return actors;
-        }
-   }
-    
-    public String insertMovie(String title, int releaseYear, String tagline) {
-    	try ( Session session = driver.session() )
-        {
-   		 
-   		 String result = session.writeTransaction( new TransactionWork<String>()
-   		 
-            {
-                @Override
-                public String execute( Transaction tx )
-                {
-                    tx.run( "CREATE (Test:Movie {title:'" + title + "', released:"+ releaseYear +", tagline:'"+ tagline +"'})");
-                    
-                    return "OK";
-                }
-            }
-   		 
-   		 );
-            
-            return result;
-        } catch (Exception e) {
-        	return e.getMessage();
+                return null;
+            });
+            greeting.get(); // Espera a que se complete la transacción
+            System.out.println("Greeting created successfully.");
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
     }
 
+    public LinkedList<String> getActors() {
+        try (Session session = driver.session()) {
+            CompletableFuture<LinkedList<String>> actors = CompletableFuture.supplyAsync(() -> {
+                LinkedList<String> myactors = new LinkedList<>();
+                try (Transaction tx = session.beginTransaction()) {
+                    Result result = tx.run("MATCH (people:Person) RETURN people.name");
+                    List<Record> registros = result.list();
+                    for (int i = 0; i < registros.size(); i++) {
+                        myactors.add(registros.get(i).get("people.name").asString());
+                    }
+                    tx.commit();
+                }
+                return myactors;
+            });
+            return actors.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public LinkedList<String> getMoviesByActor(String actor) {
+        try (Session session = driver.session()) {
+            CompletableFuture<LinkedList<String>> movies = CompletableFuture.supplyAsync(() -> {
+                LinkedList<String> mymovies = new LinkedList<>();
+                try (Transaction tx = session.beginTransaction()) {
+                    Result result = tx.run("MATCH (tom:Person {name: \"" + actor + "\"})-[:ACTED_IN]->(actorMovies) RETURN actorMovies.title");
+                    List<Record> registros = result.list();
+                    for (int i = 0; i < registros.size(); i++) {
+                        mymovies.add(registros.get(i).get("actorMovies.title").asString());
+                    }
+                    tx.commit();
+                }
+                return mymovies;
+            });
+            return movies.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String insertMovie(String title, int releaseYear, String tagline) {
+        try (Session session = driver.session()) {
+            CompletableFuture<String> result = CompletableFuture.supplyAsync(() -> {
+                try (Transaction tx = session.beginTransaction()) {
+                    tx.run("CREATE (Test:Movie {title:'" + title + "', released:" + releaseYear + ", tagline:'" + tagline + "'})");
+                    tx.commit();
+                    return "OK";
+                }
+            });
+            return result.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
+
+
+
--- src/main/webapp/js/sb-admin-2.js
+++ src/main/webapp/js/sb-admin-2.js
@@ -60,7 +60,7 @@
 		$.ajax( {
 			
 			type: "GET",
-			url: '/Grupo20/HelloServlet',
+			url: '/Grupo18-S10/HelloServlet',
 			success: function(data) {
 				//alert("Result" + data.resultado);
 			    var htmlActorsList = '<ul>';
@@ -82,7 +82,7 @@
 		$.ajax( {
 			
 			type: "GET",
-			url: '/Grupo20/MoviesByActor?actor_name=' + $('#txt-actor').val(),
+			url: '/Grupo18-S10/MoviesByActor?actor_name=' + $('#txt-actor').val(),
 			success: function(data) {
 				//alert("Result" + data.resultado);
 			    var htmlMovieList = '<ul>';
@@ -102,12 +102,12 @@
 	//Evento del botón que creara una nueva pelicula
 	$("#btn-movie-insert").click(function(){
 		
-		//alert("Resultado: " + '/Grupo20/SaveMovieServlet?title=' + $('#txt-movie-title').val() + '&release_year=' + $('#txt-movie-release_year').val() + '&tagline=' + $('#txt-movie-tagline').val());
+		//alert("Resultado: " + '/Grupo18-S10/SaveMovieServlet?title=' + $('#txt-movie-title').val() + '&release_year=' + $('#txt-movie-release_year').val() + '&tagline=' + $('#txt-movie-tagline').val());
 				
 		$.ajax( {
 			
 			type: "GET",
-			url: '/Grupo20/SaveMovieServlet?title=' + $('#txt-movie-title').val() + '&release_year=' + $('#txt-movie-release_year').val() + '&tagline=' + $('#txt-movie-tagline').val() ,
+			url: '/Grupo18-S10/SaveMovieServlet?title=' + $('#txt-movie-title').val() + '&release_year=' + $('#txt-movie-release_year').val() + '&tagline=' + $('#txt-movie-tagline').val() ,
 			success: function(data) {
 			    alert("Resultado: " + data.resultado);
 			}
